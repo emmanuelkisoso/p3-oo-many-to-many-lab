2cda1ffbcb027977c15bed5dc079d7d2bb1f94a3
emmanuel <emmanuel.kisoso@student.moringaschool.com>
2024-06-07 21:10:22 +0300

    Final Commit

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,70 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        self._contracts = []
+        Author.all.append(self)
+
+    def contracts(self):
+        return self._contracts
+
+    def add_contract(self, contract):
+        self._contracts.append(contract)
+
+    def books(self):
+        return [contract.book for contract in self._contracts]
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self._contracts)
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        self._contracts = []
+        Book.all.append(self)
+
+    def contracts(self):
+        return self._contracts
+
+    def add_contract(self, contract):
+        self._contracts.append(contract)
+
+    def authors(self):
+        return [contract.author for contract in self._contracts]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise TypeError("author must be an instance of Author")
+
+        if not isinstance(book, Book):
+            raise TypeError("book must be an instance of Book")
+
+        if not isinstance(date, str):
+            raise TypeError("date must be a string")
+
+        if not isinstance(royalties, int):
+            raise TypeError("royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+        author.add_contract(self)
+        book.add_contract(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
